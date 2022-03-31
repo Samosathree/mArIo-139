11cb89aaaae95a3ab47471db35462f02f82991fc
Samosathree <84139052+Samosathree@users.noreply.github.com>
2022-03-31 19:57:39 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,53 @@
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
 function preload() {
+	img = loadImage("mario05.png");
 	world_start = loadSound("world_start.wav");
 	setSprites();
 	MarioAnimation();
 }
 
 function setup() {
-	canvas = createCanvas(1240,336);
+	canvas = createCanvas(1240, 336);
 	canvas.parent('canvas');
 	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800, 400);
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on
+}
+
+function modelLoaded() {
+	console.log("MODEL LOADED!!!");
+}
+
+function gotPoses(results) {
+	if (results.length > 0) {
+		noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + ", noseY = " + noseY);
+	}
 }
 
 function draw() {
+	background("#7b68ee");
+	image(img, marioX, marioY, 40, 70);
 	game()
+	if(noseX > 400){
+		marioX = marioX - 1;
+	}
+	if(noseX < 800){
+		marioX = marioX + 1;
+	}
+	if(noseY > 200){
+		marioY = marioY - 1;
+	}
 }
-
-
 
 
 
